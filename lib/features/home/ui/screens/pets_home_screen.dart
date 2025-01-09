3f272ae8e798e4dff5d7a8392d9c6abbcc59b0e5
Hazem <hazemelnahas2010@gmail.com>
2025-01-09 11:43:13 +0200

    - finish header pet home screen

--- lib/features/home/ui/screens/pets_home_screen.dart
+++ lib/features/home/ui/screens/pets_home_screen.dart
@@ -1,12 +1,122 @@
 import 'package:flutter/material.dart';
 
-class PetsHomeScreen extends StatelessWidget {
+import '../../../../core/utils/const.dart';
+
+class PetsHomeScreen extends StatefulWidget {
   const PetsHomeScreen({super.key});
 
   @override
+  State<PetsHomeScreen> createState() => _PetsHomeScreenState();
+}
+
+class _PetsHomeScreenState extends State<PetsHomeScreen> {
+  int selectedCategory = 0;
+  int selectedIndex = 0;
+  List<IconData> icons = [
+    Icons.home_outlined,
+    Icons.favorite_outline_rounded,
+    Icons.chat,
+    Icons.person_outline_rounded,
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Column(
+        children: [
+           const SizedBox(height: 5),
+          headerParts(),
+          const SizedBox(height: 20),
+        ],
+      )),
+    );
+  }
+
+  Padding headerParts() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                "Location",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: black.withOpacity(0.6),
+                ),
+              ),
+              const SizedBox(width: 5),
+              const Icon(
+                Icons.keyboard_arrow_down_rounded,
+                color: blue,
+                size: 18,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const Text.rich(
+                TextSpan(
+                  text: "Mansoura, ",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "EG",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Spacer(),
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black12.withOpacity(0.03),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Icon(Icons.search),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black12.withOpacity(0.03),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Stack(
+                  children: [
+                    const Icon(Icons.notifications_outlined),
+                    Positioned(
+                      right: 5,
+                      top: 5,
+                      child: Container(
+                        height: 7,
+                        width: 7,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
